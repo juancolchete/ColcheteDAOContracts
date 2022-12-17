8525ab72005a25e9c3fb8ca80a2cc137fbc76ee2
juancolchete <juanudkp@gmail.com>
2022-12-17 09:52:26 -0300

    chore:add hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:7545"
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/"
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.13",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        }
+      }
+    ],
+  }
+};
+
+export default config;
